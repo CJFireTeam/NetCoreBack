635ae7a833fb0a7b57efedb7b80d8a59e10ab2ae
leonardoretamal <leonardo.retamal.morales@gmail.com>
2024-04-16 12:21:27 -0400

    creacion de servicios de TipoEstablecimientoSalud

---
+++ Netcore.Web.Api/DTO/NetcoreDTO/TipoEstablecimientoSaludDTO.cs
@@ -0,0 +1,16 @@
+namespace Netcore.Web.Api.DTO.NetcoreDTO
+{
+    public class TipoEstablecimientoSaludDTO 
+    {
+        public int Codigo
+        {
+            get;
+            set;
+        }
+        public string Nombre
+        {
+            get;
+            set;
+        }= null!;
+    }
+}
---
+++ Netcore.Web.Api/Services/NetCoreServices/ITipoEstablecimientoSalud.cs
@@ -0,0 +1,10 @@
+using Netcore.Web.Api.DTO.NetcoreDTO;
+
+namespace Netcore.Web.Api.Services.NetCoreServices
+{
+    public interface ITipoEstablecimientoSalud
+    {
+        Task<IResult> Get();
+
+    }
+}
