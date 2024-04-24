863e6aa87039072010a881db889141b6f3a93458
leonardoretamal <leonardo.retamal.morales@gmail.com>
2024-04-24 10:31:25 -0400

    se creo Dto,model,service, controller, business y endpoint de la tabla TipoUnidad

---
+++ Netcore.Web.Api/Model/NetcoreModel/TipoUnidadModel.cs
@@ -0,0 +1,7 @@
+using Netcore.Web.Api.DTO.NetcoreDTO;
+using Netcore.Web.Api.Model.Abstract;
+
+namespace Netcore.Web.Api.Model.NetcoreModel
+{
+    public class TipoUnidadModel : GenericBaseModel<TipoUnidadDTO> { }
+}
